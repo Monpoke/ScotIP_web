0c6d28c9ad233b13c26180e0f76669c1ac203483
Pierre Bourgeois <dragonralph@gmail.com>
2016-04-19 15:24:05 +0100

    ignore

--- src/main/java/scotip/web/pages/Contact.java
+++ src/main/java/scotip/web/pages/Contact.java
@@ -1,5 +1,7 @@
 package scotip.web.pages;
 
+import com.lyncode.jtwig.JtwigModelMap;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,10 +12,25 @@ import java.io.IOException;
  */
 public class Contact extends App {
 
+    public class Aurelien {
+        public String hello(){
+            return "AURELIEN HELLO";
+        }
+    }
+
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        this.render("pages/home/index.twig", resp);
+
+        JtwigModelMap model = new JtwigModelMap();
+        model.put("title", "Coucou je teste");
+        model.put("aurelien", new Aurelien());
+
+
+       this.render("pages/home/index.twig", resp, model);
 
     }
+
+
 }
